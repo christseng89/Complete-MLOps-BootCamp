ab5413b5fe9e8314ee611fd6094c6f5cdc2a8c55
Chris Tseng <samfire5200@gmail.com>
2024-08-21 17:09:18 +0800

    Logging Functions of Mlflow Tracking

--- MLFlow-Manage-ML-Experiments/help.py
+++ MLFlow-Manage-ML-Experiments/help.py
@@ -1,9 +1,7 @@
 import mlflow
 mlflow.set_tracking_uri("http://localhost:5000")
 
-
 exp_id = mlflow.create_experiment('Loan_Prediction')
-
 
 with mlflow.start_run(run_name='DecisionTreeClass') as run: 
     mlflow.set_tag("version","1.0.0")
@@ -12,6 +10,8 @@ mlflow.end_run()
 
 n_estimators=10
 criterion='gini'
+# mlflow_log_* => using Single Key Pair.  
+# mlflow.log_*s => using Multiple Key Pairs
 mlflow.log_param('n_estimators',n_estimators)
 mlflow.log_param('criterion',criterion)
 mlflow.log_metric('accuracy',0.9)
